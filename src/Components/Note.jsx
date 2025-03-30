c152a4cd6539ba928cd4d112ea4b21db03ce6f05
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-29 21:36:36 -0700

    Update Note & App  jsx files

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Note(){
-    return <p></p>;
+    return (
+        <div className="note">
+            <h1>This is the title.</h1>
+            <p>This is the content.</p>
+        </div>
+    )
 }
-
 export default Note;

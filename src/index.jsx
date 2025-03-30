ba01ba212b38b3ccee3dc3fcbfbababbb24a3e92
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-29 21:15:35 -0700

    Create a new React app and add Header and App components

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
 //1. Create a new React app.
+import React from "react";
+import ReactDom from "react-dom";
+import App from "../Components/App";
+
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
@@ -11,8 +15,6 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+ReactDom.render(
+    <App />, document.getElementById("root")
+)

d2302812106edcb00dcdef1082a73d0852228f06
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-03 23:37:28 -0700

    Simply the code using arrow function

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,23 +4,21 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../../notes";
 
-function createNotes(noteItem){
-    return <Note
-        key = {noteItem.key}
-        title = {noteItem.title}
-        content ={noteItem.content}
-    
-    />
-}
-
 function App(){
     return (
         <div>
             <Header />
-           {notes.map(createNotes)}
+           {notes.map((noteItem) =>
+                <Note
+                    key = {noteItem.key}
+                    title = {noteItem.title}
+                    content ={noteItem.content}
+    
+                />
+           )}
             <Footer />
         </div>
-    )
+    );
 }
 
 export default App;
